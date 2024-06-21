014a3499289793094cf7b543f7fe384fa58b84d6
MassimoGit <massimo.abacons@gmail.com>
2024-06-21 09:29:47 +0200

    Metodi di ricerca in actor repository

--- src/main/java/com/corso/videoteca/repositories/ActorRepository.java
+++ src/main/java/com/corso/videoteca/repositories/ActorRepository.java
@@ -1,7 +1,21 @@
 package com.corso.videoteca.repositories;
 
 import com.corso.videoteca.entities.Actor;
+import com.corso.videoteca.entities.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface ActorRepository extends JpaRepository<Actor, Long> {
+
+
+    Set<Actor> findByFirstnameContainsIgnoreCaseOrLastnameContainsIgnoreCaseOrderByLastnameAscFirstnameAsc(String firstname, String lastname);
+
+
+    //Dal Film troviamo il cast
+    Set<Actor> findByPlays_FilmOrderByLastnameAscFirstnameAsc(Film film);
+
+    Set<Actor> findByPlays_Film_IdOrderByLastnameAscFirstnameAsc(Long id);
+
+
 }
